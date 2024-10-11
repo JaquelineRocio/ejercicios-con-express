1eb61e49924f45a4c00c5e40be7dc02db709f360
JaquelineRocio <jaquelineramosvargas@gmail.com>
2024-10-11 00:30:53 -0500

    feat:Ejercicio 3.1: Lista de personas en la agenda

---
+++ README.md
@@ -0,0 +1,11 @@
+# Mi Primera API con Express
+
+Este proyecto es una API RestFul simple para una agenda telefónica, creada con Node.js y Express.
+
+## Instrucciones para ejecutar el proyecto
+
+1. Clona el repositorio.
+2. Instala las dependencias con `npm install`.
+3. Ejecuta el servidor en modo de desarrollo con `npm run dev`.
+
+## Endpoints
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,31 @@
-import express, { Request, Response } from "express";
-import productRoutes from "./routes/productRoutes";
+import express, { Request, Response } from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
 
 const app = express();
-const port = 3000;
+const PORT = 3001;
 
-app.use("/api", productRoutes);
+app.use(cors());
+app.use(express.json());
+app.use(morgan('tiny'));
 
-app.get("/", (_: Request, res: Response) => {
-  res.send("Hello, world!");
+interface Person {
+  id: number;
+  name: string;
+  number: string;
+}
+
+let persons: Person[] = [
+  { id: 1, name: "Arto Hellas", number: "040-123456" },
+  { id: 2, name: "Ada Lovelace", number: "39-44-5323523" },
+  { id: 3, name: "Dan Abramov", number: "12-43-234345" },
+  { id: 4, name: "Mary Poppendieck", number: "39-23-6423122" },
+];
+
+app.get('/api/persons', (req: Request, res: Response) => {
+  res.json(persons);
 });
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
